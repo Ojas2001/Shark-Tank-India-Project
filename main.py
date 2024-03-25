b3ce8dcf0e7a29189ef1432df7a02623c0cecc34
Ojas Makarand Deodhar <65889926+Ojas2001@users.noreply.github.com>
2024-03-25 23:55:26 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+import streamlit as st
+
+st.image("images/WhatsApp-Image-2023-10-18-at-3.25.30-PM-1.jpeg",width=630)
+
+with st.sidebar:
+    st.title(f'Shark Tank India')
+
+st.write("# Welcome to Shark Tank India! 👋")
+
+
+st.markdown(
+    """
+    Shark Tank India is an Indian Hindi-language business reality 
+    television series that airs on Sony Entertainment Television.
+    
+    **👈 Select a prediction from the sidebar** to see check
+    of the deal or amount pediction results !
+"""
+)
+
+
